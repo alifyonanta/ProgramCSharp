f0bdf90fca38d54d4a19503f39b7d30a62f1146f
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-11-24 19:32:21 +0700

    Create The if Statement 1.cs
    The if statement is a conditional statement that executes a block of code when a condition is true.

---
+++ The if Statement 1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 8;
+            int y = 3;
+            
+            if (x > y)
+            { 
+                Console.WriteLine("x is greater than y");
+            }
+        }
+    }
+}
