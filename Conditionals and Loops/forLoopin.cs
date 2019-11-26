a9af7887931d1f4f3726b0b9b6fa406b1bcac9c1
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-11-26 17:05:12 +0700

    Create forLoopin.cs

---
+++ Conditionals and Loops/forLoopin.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0; x < 10; x+=3)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
