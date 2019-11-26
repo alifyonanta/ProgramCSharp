5155ac53b0ad5491291f30263c311e680b16524c
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-11-26 17:02:14 +0700

    Create whileLoop.cs

---
+++ Conditionals and Loops/whileLoop.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            while(num < 6) 
+            {
+                Console.WriteLine(num);
+                num+=2;
+            }
+        }
+    }
+}
