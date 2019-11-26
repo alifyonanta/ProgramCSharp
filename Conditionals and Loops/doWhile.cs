359922a448d7f9934825bfffcb69c7ac966bcccc
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-11-26 17:06:45 +0700

    Create doWhile.cs

---
+++ Conditionals and Loops/doWhile.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 400;
+            do {
+                Console.WriteLine(x);
+                x++;
+            } while(x < 10);
+        }
+    }
+}
