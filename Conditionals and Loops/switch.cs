6534d75e4bf5994d692746ecbbbcd2d85448bcbf
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-11-26 17:00:22 +0700

    Create switch.cs

---
+++ Conditionals and Loops/switch.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 16;
+            switch (age) {
+                case 16:
+                    Console.WriteLine("Too young");
+                    break;
+                case 42:
+                    Console.WriteLine("Adult");
+                    break;
+                case 70:
+                    Console.WriteLine("Senior");
+                    break;
+                default:
+                    Console.WriteLine("The default case");
+                    break;
+            }
+        }
+    }
+}
