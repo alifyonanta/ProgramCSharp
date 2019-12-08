7d563b6cf2ac8448c9e6e7dcde9e6c7f6ba31057
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-12-08 22:58:34 +0700

    Create Constant.cs

---
+++ More on Classes/Constant.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        class MathClass {
+            public const int ONE = 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write(MathClass.ONE);
+        }
+    }
+}
