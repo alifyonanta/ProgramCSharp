123cbaa3078efa3906293397502fb1403c6975ff
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-12-08 23:01:12 +0700

    Create Static Constructors.cs

---
+++ More on Classes/Static Constructors.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        class SomeClass {
+            public static int X { get; set; }
+            public static int Y { get; set; }
+            
+            static SomeClass() {
+                X = 10;
+                Y = 20;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SomeClass.X);
+        }
+    }
+}
