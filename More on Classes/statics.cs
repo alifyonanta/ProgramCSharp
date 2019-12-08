d695c3aae66780338ad188535b79217ee7031527
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2019-12-08 17:11:01 +0700

    Create statics.cs

---
+++ More on Classes/statics.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        class Sapi
+        {
+            public static void Perah() {
+                Console.WriteLine("ini sapi perah");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Sapi.Perah();
+        }
+    }
+}
