393f7c69a15d77d90736485f9c2272ce25c1cfc2
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2020-01-07 14:34:05 +0700

    Create struct.cs

---
+++ Structs, Enums, Exceptions & Files/struct.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        struct Book {
+            public string title;  
+            public double price;
+            public string author;
+        }
+        static void Main(string[] args)
+        {
+            Book b;
+            b.title = "Test";
+            b.price = 5.99;
+            b.author = "David";
+            
+            Console.WriteLine(b.title);
+            Console.WriteLine(b.price);
+        }
+    }
+}
