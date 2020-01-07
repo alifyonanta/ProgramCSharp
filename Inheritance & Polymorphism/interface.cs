d57a30c5e245026e0e581d4c384e653e666cae99
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2020-01-07 14:10:25 +0700

    Create interface.cs

---
+++ Inheritance & Polymorphism/interface.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        public interface IShape {
+            void Draw();
+        }
+        class Circle : IShape {
+            public void Draw() {
+                Console.WriteLine("Circle Draw");
+            }
+        }
+        static void Main(string[] args)
+        {
+            IShape c = new Circle();
+            c.Draw();
+        }
+    }
+}
